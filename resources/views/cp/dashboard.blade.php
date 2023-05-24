2abb4317b6fbf340f164183921d6fcfa28396c3c
faisalach <faisalachramsyah@gmail.com>
2023-05-24 18:35:53 +0700

    logout

--- resources/views/cp/dashboard.blade.php
+++ resources/views/cp/dashboard.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     @include("partials/header")
-    <title>Document</title>
+    <title>Dashboard</title>
 </head>
 <body>
     <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -52,7 +52,7 @@
                  </a>
               </li>
               <li>
-                 <a href="#" class="flex items-center p-2 text-red-500 rounded-lg dark:text-red hover:bg-gray-100 dark:hover:bg-gray-700">
+                 <a href="{{ url("/logout") }}" class="flex items-center p-2 text-red-500 rounded-lg dark:text-red hover:bg-gray-100 dark:hover:bg-gray-700">
                     <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-red-500 transition duration-75 dark:text-red-400 group-hover:text-red-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
                     <span class="flex-1 ml-3 whitespace-nowrap">Logout</span>
                  </a>
