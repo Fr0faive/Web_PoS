69c15f1d6f48cb926714f9c551451318545fd9cd
unknown <faikar.moch.tajudin@gmail.com>
2023-05-24 07:28:04 +0700

    edit nama db, pembuatan dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,26 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="">
-    <title>Document</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Login - PoS</title>
 </head>
 <body class="bg-bg-login">
     <section class="h-full w-full">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-xl font-semibold text-gray-900 dark:text-white">
-                <img class="w-12 h-12 mr-1" src="{{ asset('/images/logo.svg') }}" alt="logo">
-                Kelompok 1
+        <div class="flex flex-col items-center justify-center px-6 py-24 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-6 text-3xl font-semibold text-gray-900 dark:text-white">
+                <img class="w-20 h-20 mr-1" src="{{ asset('/images/logo.svg') }}" alt="logo">
+                Point of Sales
             </a>
             <div class="w-full rounded-lg shadow-md dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 bg-gray-100 backdrop-filter backdrop-blur-md bg-opacity-20">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign in to your account
-                    </h1>
+                    <div>
+                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Masuk
+                        </h1>
+                        <p class="text-base font-normal py-2">Gunakan akun pegawai anda</p>
+                    </div>
                     <form class="space-y-4 md:space-y-6" action="#" id="formLogin" method="POST">
                         @csrf
                         <div>
                             <label for="nomor_pegawai" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Pegawai</label>
-                            <input type="text" name="nomor_pegawai" id="nomor_pegawai" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="">
+                            <input type="text" name="nomor_pegawai" id="nomor_pegawai" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="023xxxxxxxx" required="">
                         </div>
                         <div>
                             <label for="password_akun" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -45,6 +48,7 @@
                     </form>
                 </div>
             </div>
+            <p class="flex item-center mt-4 text-xs font-light">© Kelompok 1</p>
         </div>
       </section>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
