442b1d5732dc727b08fcac3dd7b3f5e91a2d22f4
faisalach <faisalachramsyah@gmail.com>
2023-06-17 16:39:43 +0700

    Nambah menu produk, produk kategori dan supplier

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -64,6 +64,27 @@
                 </a>
             </li>
             <li>
+                <a href="{{ route('cp.product') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img src="{{ asset('assets/icon-user.svg') }}" alt="icon-user" class="w-7 h-7">
+                    <span class="flex-1 ml-3 whitespace-nowrap">Data Produk</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('cp.product_category') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img src="{{ asset('assets/icon-user.svg') }}" alt="icon-user" class="w-7 h-7">
+                    <span class="flex-1 ml-3 whitespace-nowrap">Data Produk Kategori</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('cp.supplier') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img src="{{ asset('assets/icon-user.svg') }}" alt="icon-user" class="w-7 h-7">
+                    <span class="flex-1 ml-3 whitespace-nowrap">Data Supplier</span>
+                </a>
+            </li>
+            <li>
                 <a href="#"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                     <img src="{{ asset('assets/icon-presensi.svg') }}" alt="icon-user" class="w-7 h-7">
