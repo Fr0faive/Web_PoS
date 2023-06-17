c18cb87d317ee64bf6adc02b2178f9608f8b288b
unknown <faikar.moch.tajudin@gmail.com>
2023-06-17 15:52:46 +0700

    perbaiki ulang

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -13,7 +13,7 @@
     class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
     aria-label="Sidebar">
     <div
-        class="h-full px-3 py-4 overflow-y-auto rounded-r-lg shadow-md bg-blue-300 backdrop-filter backdrop-blur-md bg-opacity-20 dark:bg-gray-800">
+        class="h-full px-3 py-4 overflow-y-auto rounded-r-lg shadow-md bg-blue-300 backdrop-filter backdrop-blur-md bg-opacity-40 dark:bg-gray-800">
         <a href="{{ route('cp.dashboard') }}" class="flex items-center mb-6 text-xl font-semibold text-gray-900 dark:text-white">
             <img class="w-20 h-20 mr-1" src="{{ asset('/images/logo.svg') }}" alt="logo">
             Point of Sales
@@ -21,8 +21,8 @@
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="{{ route('cp.dashboard') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg focus:bg-gray-500 focus:text-white dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700">
-                    <img src="{{ asset('assets/icon-user.svg') }}" alt="icon-user" class="w-7 h-7 bg-white rounded">
+                    class="flex items-center p-2 text-gray-900 rounded-lg focus:bg-gray-100 focus:text-white dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img src="{{ asset('assets/icon-db.svg') }}" alt="icon-user" class="w-7 h-7">
                     <span class="ml-3">Dashboard</span>
                 </a>
             </li>
@@ -30,13 +30,7 @@
                 <button type="button"
                     class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                     aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                    <svg aria-hidden="true"
-                        class="flex-shrink-0 w-6 h-6 text-blue-500 transition duration-75 group-hover:text-blue-900 dark:text-gray-400 dark:group-hover:text-white"
-                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                    <img src="{{ asset('assets/icon-pro.svg') }}" alt="icon-user" class="w-7 h-7">
                     <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Produk</span>
                     <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
@@ -65,25 +59,14 @@
             <li>
                 <a href="{{ route('cp.pegawai') }}"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <svg aria-hidden="true"
-                        class="flex-shrink-0 w-6 h-6 text-blue-500 transition duration-75 dark:text-blue-400 group-hover:text-blue-900 dark:group-hover:text-white"
-                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                    <img src="{{ asset('assets/icon-user.svg') }}" alt="icon-user" class="w-7 h-7">
                     <span class="flex-1 ml-3 whitespace-nowrap">Data Pegawai</span>
                 </a>
             </li>
             <li>
                 <a href="#"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <svg aria-hidden="true"
-                        class="flex-shrink-0 w-6 h-6 text-blue-500 transition duration-75 dark:text-blue-400 group-hover:text-blue-900 dark:group-hover:text-white"
-                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                    <img src="{{ asset('assets/icon-presensi.svg') }}" alt="icon-user" class="w-7 h-7">
                     <span class="flex-1 ml-3 whitespace-nowrap">Absensi</span>
                 </a>
             </li>
