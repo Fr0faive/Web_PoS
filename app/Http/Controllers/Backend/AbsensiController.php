3477d185efb7f832a48d5f64fec41b58e29d18fe
faisalach <faisalachramsyah@gmail.com>
2023-06-18 09:30:41 +0700

    absensi

---
+++ app/Http/Controllers/Backend/AbsensiController.php
@@ -0,0 +1,79 @@
+<?php
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Absensi;
+use DataTables;
+
+class AbsensiController extends Controller
+{
+    public function datatable(Request $request) {
+        if(Auth::guard("admin")->check()){
+            $data   = Absensi::join("pegawai","pegawai.id_pegawai","=","absensi.id_pegawai")
+            ->select("pegawai.nama_pegawai","pegawai.nomor_pegawai","tanggal_masuk","tanggal_keluar")
+            ->get();
+        }else{
+            $id_pegawai = \AppHelper::userLogin()->id_pegawai;
+            $data   = Absensi::join("pegawai","pegawai.id_pegawai","=","absensi.id_pegawai")
+            ->select("pegawai.nama_pegawai","pegawai.nomor_pegawai","tanggal_masuk","tanggal_keluar")
+            ->where("absensi.id_pegawai",$id_pegawai)
+            ->get();
+        }
+        return DataTables::of($data)->make(true);
+    }
+    public function insertAbsensi(Request $request) {
+        $status     = "";
+        $message     = "";
+        $validation_error   = false;
+
+        $absensi_today  = \AppHelper::absensiToday();
+
+        $action     = !empty($request->action) ? $request->action : "masuk";
+        if(!$validation_error){
+            if($action == "masuk" && !empty($absensi_today)){
+                $status     = "error";
+                $message    = "Anda telah melakukan absensi masuk hari ini.";
+                $validation_error   = true;
+            }elseif($action == "keluar" && empty($absensi_today)){
+                $status     = "error";
+                $message    = "Anda belum melakukan absensi masuk hari ini.";
+                $validation_error   = true;
+            }elseif($action == "keluar" && !empty($absensi_today->tanggal_keluar)){
+                $status     = "error";
+                $message    = "Anda telah melakukan absensi keluar hari ini.";
+                $validation_error   = true;
+            }
+        }
+
+        if (!$validation_error){
+            $id_pegawai = \AppHelper::userLogin()->id_pegawai;
+            if($action == "masuk"){
+                $absensi   = new Absensi;
+                $absensi->id_pegawai = $id_pegawai;
+                $absensi->tanggal_masuk = date("Y-m-d H:i:s");
+            }elseif($action == "keluar"){
+                $absensi   = Absensi::find($absensi_today->id_absensi);
+                $absensi->tanggal_keluar = date("Y-m-d H:i:s");
+            }
+    
+            if($absensi->save()){
+                $status     = "success";
+                $message    = "Absensi berhasil diinput";
+            }else{
+                $status     = "error";
+                $message    = "Absensi gagal diinput";
+            }
+        }
+
+        $response = [
+            "status" => $status,
+            "message" => $message,
+        ];
+        return response()->json($response);
+    }
+
+}
---
+++ resources/views/cp/absensi.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include("partials/header")
+    <title>Produk Kategori</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+    <link rel="stylesheet" href="{{ url('/assets/custom_datatable.css') }}">
+</head>
+
+<body class="bg-dashboard bg-cover">
+
+    @include("partials.sidebar")
+    @csrf
+    
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 bg-gray-50 rounded shadow-md ">
+            @auth("kasir")
+            <button class="btn_absensi_masuk text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                Absensi Masuk
+            </button>
+
+            <button class="btn_absensi_keluar text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" type="button">
+                Absensi Keluar
+            </button>
+            @endauth
+
+            <div class="relative rounded-lg">
+                <div class="p-2 mt-2 rounded-lg ">
+                <table id="dataTable">
+                    <thead class="bg-gray-100">
+                        <tr>
+                            <td scope="col">
+                                Nama Pegawai
+                            </td>
+                            <td scope="col">
+                                Nomor Pegawai
+                            </td>
+                            <td scope="col">
+                                Tanggal Masuk
+                            </td>
+                            <td scope="col">
+                                Tanggal Keluar
+                            </td>
+                        </tr>
+                    </thead>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>  
+
+    <script>
+        $(document).ready(function () {
+            $('#dataTable').DataTable({
+                serverside : true,
+                processing : true,
+                ajax    : {
+                    url     : "{{ route('absensi.datatable') }}",
+                },
+                columns: [
+                    { data: 'nama_pegawai', name : 'nama_pegawai' },
+                    { data: 'nomor_pegawai', name : 'nomor_pegawai' },
+                    { data: 'tanggal_masuk', name : 'tanggal_masuk' },
+                    { data: 'tanggal_keluar', name : 'tanggal_keluar' },
+                ],
+                order : [[2,"desc"]]
+            });
+
+            $("body").on("click",".btn_absensi_masuk",function(e){
+                e.preventDefault();
+                if(confirm("Absensi Masuk?")){
+                    $.ajax({
+                        url     : `{{ route("absensi.insert") }}`,
+                        data    : {
+                            _token : $("[name=_token]").val(),
+                            action  : "masuk"
+                        },
+                        method  : "POST",
+                        dataType  : "JSON",
+                        success : function(data){
+                            alert(data.message);
+                            if(data.status == "success"){
+                                $("#dataTable").DataTable().ajax.reload();
+                            }
+                        }
+                    })
+                }
+            })
+
+            $("body").on("click",".btn_absensi_keluar",function(e){
+                e.preventDefault();
+                if(confirm("Absensi Keluar?")){
+                    $.ajax({
+                        url     : `{{ route("absensi.insert") }}`,
+                        data    : {
+                            _token : $("[name=_token]").val(),
+                            action  : "keluar"
+                        },
+                        method  : "POST",
+                        dataType  : "JSON",
+                        success : function(data){
+                            alert(data.message);
+                            
+                            if(data.status == "success"){
+                                $("#dataTable").DataTable().ajax.reload();
+                            }
+                        }
+                    })
+                }
+            })
+
+        });
+    </script>
+</body>
+
+</html>
