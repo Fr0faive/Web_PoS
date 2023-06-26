3dfec30bc243c6627010cba0511923017077cd50
unknown <faikar.moch.tajudin@gmail.com>
2023-06-26 17:45:29 +0700

    ubah posisi button

--- resources/views/cp/detail_invoice.blade.php
+++ resources/views/cp/detail_invoice.blade.php
@@ -5,7 +5,7 @@
     <title>Invoice Details</title>
     <style>
         @media print{
-            
+
             .btnPrint{
                 display: none;
             }
@@ -14,6 +14,7 @@
 </head>
 <body>
     <div class="mx-auto">
+        <button type="button" onclick="window.print()" class="btnPrint mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Print</button>
         <h3 class="text-center text-3xl font-bold mt-10">INVOICE</h3>
         <div class="mt-5 ml-3">
             <table>
@@ -22,11 +23,11 @@
                     <td class="px-3 py-2">: {{ $penjualan->nomor_invoice }}</td>
                 </tr>
                 <tr>
+
                     <td class="px-3 py-2">Tanggal Transaksi</td>
                     <td class="px-3 py-2">: {{ $penjualan->tanggal_penjualan }}</td>
                 </tr>
             </table>
-            <button type="button" onclick="window.print()" class="btnPrint mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Print</button>
         </div>
         <div class="relative overflow-x-auto mt-3">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
